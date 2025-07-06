ad24527c6131f2b82d52ee06551e5146e572b7b5
Fernando Putallaz <fernando.putallaz@foxbox.com>
2025-07-06 19:48:59 -0300

    Add Pod demo

---
+++ Examples/FreshpaintPodDemo/FreshpaintPodDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FreshpaintPodDemo
+//
+//  Created by Fernando Putallaz on 06/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Examples/FreshpaintPodDemo/FreshpaintPodDemo/FreshpaintPodDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreshpaintPodDemoApp.swift
+//  FreshpaintPodDemo
+//
+//  Created by Fernando Putallaz on 06/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct FreshpaintPodDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
